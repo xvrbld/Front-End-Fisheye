64c74e9151baec0914414e6ae91135fa99d37150
Xavier Baldacchino <xbaldacchino@gmail.com>
2022-08-11 09:54:27 +0200

    city + country

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait, city , country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -8,20 +8,24 @@ function photographerFactory(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
-        h2.setAttribute("class","name");
+        h2.setAttribute( 'class' , 'name');
         h2.textContent = name;
-        const location = document.createElement ( 'span' );
-        location.setAttribute("class","location");
-        location.textContent = city , country;
+        const locCity = document.createElement ( 'span' );
+        locCity.setAttribute( 'class' , 'city' );
+        locCity.textContent = city;
+        const locCountry = document.createElement ( 'span' );
+        locCountry.setAttribute( 'class' , 'country' );
+        locCountry.textContent = country;
         const taglineEl = document.createElement ( 'span' );
-        taglineEl.setAttribute("class","tagline");
+        taglineEl.setAttribute( 'class' , 'tagline' );
         taglineEl.textContent = tagline;
         const priceTag = document.createElement ( 'span' );
-        priceTag.setAttribute("class","price");
+        priceTag.setAttribute( 'class' , 'price' );
         priceTag.textContent = price;
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(location);
+        article.appendChild(locCity);
+        article.appendChild(locCountry);
         article.appendChild(taglineEl);
         article.appendChild(priceTag);
         return (article);
