31bddd9af8b22d8be843e3d72a8e14763270e68e
Xavier Baldacchino <xbaldacchino@gmail.com>
2022-08-18 13:47:20 +0200

    photographer CSS

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,68 +1,71 @@
 //Mettre le code JavaScript lié à la page photographer.html
 
 async function displayData(photographer) {
-    document.getElementById('name').innerText = photographer.name;
-    document.getElementById('location').innerText = `${photographer.city}, ${photographer.country}`;
-    document.getElementById('tagline').innerText = photographer.tagline;
-    const picture = `assets/photographers/${photographer.portrait}`;
-    document.getElementById('photographer-profile').src = picture
-    document.getElementById('price').innerText = `${photographer.price}€ / jour`;
+  document.getElementById("name").innerText = photographer.name;
+  document.getElementById(
+    "location"
+  ).innerText = `${photographer.city}, ${photographer.country}`;
+  document.getElementById("tagline").innerText = photographer.tagline;
+  const picture = `assets/photographers/${photographer.portrait}`;
+  document.getElementById("photographer-profile").src = picture;
+  document.getElementById("price").innerText = `${photographer.price}€ / jour`;
 }
 
 async function displayMedias(mediasPhotographer) {
-    const gallery = document.getElementById('gallery');
-    let mediasHtml = '';
-    let nbLikesTotal = 0;
-    mediasPhotographer.map(media => {
-        nbLikesTotal += media.likes;
-        let imageOrVideo;
-        if(media.image) {
-            imageOrVideo = `<a href="javascript:void(0)"><img class="sample-photo" src="assets/photos/${media.image}" /></a>`;
-        } else {
-            imageOrVideo = `<a href="javascript:void(0)"><video class="sample-photo" src="assets/photos/${media.video}"></video></a>`;
-        }
+  const gallery = document.getElementById("gallery");
+  let mediasHtml = "";
+  let nbLikesTotal = 0;
+  mediasPhotographer.map((media) => {
+    nbLikesTotal += media.likes;
+    let imageOrVideo;
+    if (media.image) {
+      imageOrVideo = `<a href="javascript:void(0)"><img class="sample-photo" src="assets/photos/${media.image}" /></a>`;
+    } else {
+      imageOrVideo = `<a href="javascript:void(0)"><video class="sample-photo" src="assets/photos/${media.video}"></video></a>`;
+    }
 
-
-        mediasHtml = mediasHtml + `<div class="photo-card">
+    mediasHtml =
+      mediasHtml +
+      `<div class="photo-card">
         ${imageOrVideo}
         <div class="desc">
-          <span>${media.title}</span>
+          <p>${media.title}</p>
           <a onclick="addLike(this)" href="javascript:void(0)"><span class="likes">${media.likes}</span><i class="fa-solid fa-heart"></i></a>
         </div>
       </div>`;
-    });
-    gallery.innerHTML = mediasHtml;
-    document.getElementById('likesTotal').innerText = nbLikesTotal;
+  });
+  gallery.innerHTML = mediasHtml;
+  document.getElementById("likesTotal").innerText = nbLikesTotal;
 }
 
 async function addLike(el) {
-    let nbLikes = parseInt(el.querySelector('.likes').innerText);
-    nbLikes++;
-    el.querySelector('.likes').innerText = nbLikes;
-    let nbLikesTotal = parseInt(document.getElementById('likesTotal').innerText);
-    nbLikesTotal++;
-    document.getElementById('likesTotal').innerText = nbLikesTotal;
-} 
+  let nbLikes = parseInt(el.querySelector(".likes").innerText);
+  nbLikes++;
+  el.querySelector(".likes").innerText = nbLikes;
+  let nbLikesTotal = parseInt(document.getElementById("likesTotal").innerText);
+  nbLikesTotal++;
+  document.getElementById("likesTotal").innerText = nbLikesTotal;
+}
 
 async function getPhotographerById() {
-    let paramsUrlProfile = new URLSearchParams(window.location.search);
-    let id = paramsUrlProfile.get('id');
-    console.log(id);
-    await fetch("./data/photographers.json")
-	.then(reponse => reponse.json())
-	.then((data) => (photographers = data.photographers))
-    console.log(photographers);
-    const photographer = photographers.find(item => item.id == id);
-    console.log(photographer);
+  let paramsUrlProfile = new URLSearchParams(window.location.search);
+  let id = paramsUrlProfile.get("id");
+  console.log(id);
+  await fetch("./data/photographers.json")
+    .then((reponse) => reponse.json())
+    .then((data) => (photographers = data.photographers));
+  console.log(photographers);
+  const photographer = photographers.find((item) => item.id == id);
+  console.log(photographer);
 
-    await fetch("./data/photographers.json")
-	.then(reponse => reponse.json())
-	.then((data) => (medias = data.medias))
-    const mediasPhotographer = medias.filter(item => item.photographerId == id);
-    console.log(mediasPhotographer);
+  await fetch("./data/photographers.json")
+    .then((reponse) => reponse.json())
+    .then((data) => (medias = data.medias));
+  const mediasPhotographer = medias.filter((item) => item.photographerId == id);
+  console.log(mediasPhotographer);
 
-    await displayData(photographer);
-    await displayMedias(mediasPhotographer);
+  await displayData(photographer);
+  await displayMedias(mediasPhotographer);
 }
 
 getPhotographerById();
