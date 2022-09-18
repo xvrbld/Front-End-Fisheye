73bf4c7e0db3093888183a3d6e4fd5d44b32e181
Xavier Baldacchino <xbaldacchino@gmail.com>
2022-09-18 21:17:45 +0200

    eslint modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 function displayModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "block";
 }
 
+// eslint-disable-next-line no-unused-vars
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
